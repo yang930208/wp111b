9ef934129f4274181c0f40bb57b814b5c510234f
yang930208 <a0905009317@gmail.com>
2023-04-28 11:43:15 +0800

    ..

--- homework09/1.js
+++ homework09/1.js
@@ -8,9 +8,10 @@ let pages = {
     <h1>登入</h1>
     <input type="text" placeholder="帳號"/><br/>
     <input type="password" placeholder="密碼"/><br/>
+    <input type="button" value="登入"/><br/>
     `,
     '#logout':`
-    <p>您已登出成功</p> 
+    <p>您已登出成功</p>
     `,
     '#signup':`
     <h1>註冊</h1>
@@ -20,10 +21,11 @@ let pages = {
     <input type="email" placeholder="email"/><br/>
     <input type="text" placeholder="血型"/><br/>
     <input type="date" placeholder="生日"/><br/>
+    <input type="button" value="註冊"/><br/>
     `,
     '#home':`
     <h1>首頁</h1>
-    <p>本網頁可以按功能表去切換畫面</p> 
+    <p>本網頁可以按功能表去切換畫面</p>
     `,
 }
 window.onhashchange = function () {
